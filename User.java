4c3045ccb1eaa754999e7ca7de7535775dff560c
kalpadiptyaroy <40118928+kalpadiptyaroy@users.noreply.github.com>
2022-07-18 13:00:57 +0530

    Initial Commit

---
+++ User.java
@@ -0,0 +1,31 @@
+//Project Springwebmvc
+
+
+package springwebmvc;
+
+import org.springframework.stereotype.Service;
+
+
+@Service("user")
+public class User {
+    String userName;
+    String password;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
+}
